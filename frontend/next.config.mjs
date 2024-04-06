5c746bec97bb5a298c2de341fcdd12cfcce5efc0
mirrorgo <cloudmirrorgo@163.com>
2024-04-06 15:05:49 +1100

    feat: server http support

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://server.unimelb.top:8081/:path*", // 将请求代理到后端
+        // destination: "http://localhost:3001/api/:path*", // 将请求代理到后端
+        // http://67.219.111.154:8090/api/user/login
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- frontend/src/service/requestUtils.ts
+++ frontend/src/service/requestUtils.ts
@@ -1,8 +1,7 @@
 const requestUrl = (url: string, useMock: boolean = false) => {
   const mockUrl = "https://mock.apifox.com/m1/4275689-3917637-default";
   if (!useMock) {
-    // return `/api${url}`;
-    return `http://67.219.111.154:8081${url}`;
+    return `/api${url}`;
   } else {
     return mockUrl + url;
   }
